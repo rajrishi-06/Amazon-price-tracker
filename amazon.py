94fdf55cf65bbf99fab41b93a3d38efd5fd83522
rajrishi-06 <rajrishireddyk@gmail.com>
2024-12-26 13:40:37 +0530

    Initial commit

--- amazon.py
+++ amazon.py
@@ -6,8 +6,12 @@ class Amazon:
         self.target = expecting
         self.product_name = ""
         self.current_price = 0
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+            "Accept-Language" : "en-GB,en-US;q=0.9,en;q=0.8"
+        }
     def scrape_product(self):
-        response = requests.get(url=self.url)
+        response = requests.get(url=self.url, headers=self.headers)
         content = response.text
         soup = BeautifulSoup(content, "html.parser")
         self.product_name =" ".join(soup.select_one("div#titleSection span#productTitle").getText().split())
